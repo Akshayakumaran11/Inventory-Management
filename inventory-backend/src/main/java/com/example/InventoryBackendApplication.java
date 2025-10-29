4dd89b38a77a2c9db2c05dd0c34620587e58a97b
Akshaya <akshayakumaran2002@gmail.com>
2025-10-29 17:31:14 +0530

    first commit

---
+++ inventory-backend/src/main/java/com/example/InventoryBackendApplication.java
@@ -0,0 +1,12 @@
+package com.example.inventorybackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InventoryBackendApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(InventoryBackendApplication.class, args);
+        System.out.println("Inventory backend started on port 8080");
+    }
+}
